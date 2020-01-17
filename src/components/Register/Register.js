110d300d612f296fa7b2d735a81bbe5a8e0b0d38
kaiorosa1 <kaiosrosa@gmail.com>
2020-01-16 22:41:05 -0300

    add more components

---
+++ src/components/Register/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './Logo.css';
+
+const Register = () =>{
+    return (
+        <div className="logo">
+            <p>Register</p>
+        </div>
+    );
+}
+
+export default Register;
---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './Logo.css';
+
+const SignIn = () =>{
+    return (
+        <div className="logo">
+            <p>SignIn</p>
+        </div>
+    );
+}
+
+export default SignIn;
