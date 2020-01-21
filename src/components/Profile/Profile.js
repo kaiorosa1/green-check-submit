bbd04b802888b4187e26db7506248752da0fdd66
kaiorosa1 <kaiosrosa@gmail.com>
2020-01-20 21:28:34 -0300

    add changes Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Profile = () =>{
     return (
         <div>
-            <h1>Kaio : </h1>
-            <p>You got # answers right!</p>
-            <p> You're at #1 from #5 </p>
-            <p>submit more</p>
+            <h1>{'Kaio : '}</h1>
+            <p>{'You got # answers right!'}</p>
+            <p> {'You\'re at #1 from #5'} </p>
+            <p>{'submit more'}</p>
         </div>
     );
 }
