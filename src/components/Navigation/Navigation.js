f07dafe9cb02e4ad9ff0cf70307e57d2e53d2576
kaiorosa1 <kaiosrosa@gmail.com>
2020-01-20 21:35:34 -0300

    add padding and margin to Nav bar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import './Navigation.css';
 
 const Navigation = () =>{
     return (
-        <div className="nav-links">
+        <div className="nav-links f3 pa3 ma3">
             <a href="#">Profile</a>
             <a href="#">Register</a>
             <a href="#">SignIn</a>
